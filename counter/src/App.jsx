c776b77527e7fe9d6ad9d739b7c06091cb9102f3
Aashi <aashitripathi2002@gmail.com>
2025-03-17 13:09:01 +0530

    concept of useState

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -11,8 +11,30 @@ const addValue=() =>{
   console.log("clicked",Math.random())
  //  counter=counter+1;
  if(counter<20){
- counter=counter+1;
- setCounter(counter)
+//  counter=counter+1;
+//  setCounter(counter)
+setCounter((prevCounter)=>prevCounter+1)
+setCounter((prevCounter)=>prevCounter+1)
+setCounter((prevCounter)=>prevCounter+1)
+setCounter((prevCounter)=>prevCounter+1)
+
+// All 4 updates will be applied one after another, and the counter will increase by 4.
+// So if the current value is 15, it will become: 👉 15 → 16 → 17 → 18 → 19
+// This means:
+// "Hey React, don't just overwrite the counter with a value.
+// Instead, give me the latest value, and I’ll tell you how to update it.
+// So each update will use the most up-to-date value, even if multiple setCounter() calls are stacked one after another.
+// Why don't we need to declare prevCounter?
+// prevCounter is just a parameter — like a function argument.
+// React automatically passes the latest value of the state into that function.
+// What if you wrote it like this instead?
+// setCounter(counter + 1)
+// setCounter(counter + 1)
+// setCounter(counter + 1)
+// setCounter(counter + 1)
+//  This won’t work the same. The counter will only go up by 1.
+// Why? Because all 4 lines use the same old value of counter. React batches these updates and thinks:
+// "Oh, you're telling me to set it to 16 four times? Cool, I’ll just do it once."
  }
  else
  {
