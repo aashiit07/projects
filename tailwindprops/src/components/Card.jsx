9104f6dd9ee011bfab1d5494592cacb4b3709a95
Aashi <aashitripathi2002@gmail.com>
2025-03-17 12:42:24 +0530

    used tailwind and props

--- tailwindprops/src/components/Card.jsx
+++ tailwindprops/src/components/Card.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/Card'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Card({username}) {
   return (
-    <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4" >Tailwind test</h1>
-      {/* <div className="w-80 flex flex-col rounded-xl bg-black min-h-[2rem] top-2 mb-4 ">
+    <div className="w-80 flex flex-col rounded-xl bg-black min-h-[2rem] top-2 mb-4 ">
         <div>
           <img
             src="https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg"
@@ -20,7 +12,7 @@ function App() {
         </div>
         <div className="flex flex-col py-3 px-3 pb-10">
           <div className="flex justify-between ">
-            <h1 className="font-bold ">Bored </h1>
+            <h1 className="font-bold ">{username} </h1>
             <h1>Price</h1>
           </div>
           <div className="flex  justify-between">
@@ -28,14 +20,8 @@ function App() {
             <p>0.01</p>
           </div>
         </div>
-      </div> */}
-      <Card username="Aashi"/>
-      <Card username="Ast"/>
-      <Card username="Akd"/>
-        
-    
-    </>
+      </div>
   )
 }
 
-export default App
+export default Card
